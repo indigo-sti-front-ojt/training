f2a40f109060e4dc94c0699a0903fefcd3b21a0d
ReiRyuSTI <ry-tanaka@sios.com>
2022-07-07 14:32:23 +0900

    Axiosの共通化処理

--- react_training_ryu_axios_unstated_next/src/App.tsx
+++ react_training_ryu_axios_unstated_next/src/App.tsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./App.css";
+import { axiosClient } from "./axios/axiosClient";
 
 function App() {
-  return <div className="App"></div>;
+  const onClickMethodTest_0 = async () => {
+    try {
+      const result = await axiosClient.get("/posts");
+      console.log(result);
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
+  const onClickMethodTest_1 = async () => {
+    try {
+      const result = await axiosClient.get("/posts", {
+        params: {
+          id: 0,
+        },
+      });
+      console.log(result);
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
+  const onClickMethodTest_2 = async () => {
+    try {
+      const result = await axiosClient.post("/posts", {
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      });
+      console.log(result);
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickMethodTest_0}>all get</button>
+      <button onClick={onClickMethodTest_1}>get</button>
+      <button onClick={onClickMethodTest_2}>post</button>
+    </div>
+  );
 }
 
 export default App;
