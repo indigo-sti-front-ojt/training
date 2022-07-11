a72a4497ac511ae95e44b6ae3b69f0151a8f86e4
ChinaSasaki <c-sasaki@sios.com>
2022-07-11 19:13:40 +0900

    firebaseプロジェクトを削除

--- react_china/chakra-demo/src/hooks/useAllUsers.tsx
+++ react_china/chakra-demo/src/hooks/useAllUsers.tsx
@@ -27,16 +27,21 @@ export const useAllusers = () => {
 
 
   // async/await構文を使った書き方
-const getUsers = useCallback(async() => {
-  try{
-    const res = await axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users/");
-    setUsers(res.data);
-  }catch(error){
-    showMessage({ title: "ユーザーが取得できません", status: "error" });
-  }finally{
-    setLoading(false);
-  }
-},[]);
+  const getUsers = useCallback(()=>{
+    (async() => {
+      try{
+        const res = await axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users/");
+        setUsers(res.data);
+      }catch(error){
+        showMessage({ title: "ユーザーが取得できません", status: "error" });
+      }finally{
+        setLoading(false);
+      }
+    })()
+    // 非同期処理の関数をuseEffectに入れる場合は通常Promise型で返されてしまう
+    // それを防ぐために関数の中で定数として関数を定義する
+    // 上記のように()に入れて定義して、空の()で関数を実行する
+  },[]);
 
   return { getUsers,loading,users };
 };
