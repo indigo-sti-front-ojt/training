7848c4c2311b5000cce0388f78b339643457bf7b
ReiRyuSTI <ry-tanaka@sios.com>
2022-07-15 11:34:14 +0900

    react-jsonschema-formの動作確認

--- react_jsonschema_ryu/src/App.tsx
+++ react_jsonschema_ryu/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { TestSample } from "./views/TestSample";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TestSample />
+    </div>
+  );
 }
 
 export default App;
