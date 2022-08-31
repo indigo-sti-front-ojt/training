38723fa82c13784387e62867d0eb090aecbfe649
ChinaSasaki <c-sasaki@sios.com>
2022-08-31 11:11:21 +0900

    ログインのバグを修正

--- react_china/reacr-typescript-firebase-router-with-jserver/src/hooks/useAuth.ts
+++ react_china/reacr-typescript-firebase-router-with-jserver/src/hooks/useAuth.ts
@@ -13,6 +13,8 @@ import {
 import { useUserCreateEdit } from "./api/postPutDelete/useUserCreateEdit";
 import { UserMinInfo } from "../types/api/UserMinInfo";
 import { useLoginUserContext } from "../context/LoginUserContext";
+import { useUser } from "./api/get/useUser";
+import { useUserInfoContext } from "../context/UserInfoContext";
 
 const fireauth = firebaseApp.fireauth;
 
@@ -20,7 +22,12 @@ const fireauth = firebaseApp.fireauth;
 export const useLoginWithGoogle = () => {
   const navigate = useNavigate();
 
+  // ユーザー作成apihooks
   const { userCreateEdit } = useUserCreateEdit();
+
+  // ユーザDB確認のためのユーザコンテキスト更新用hooks と ユーザコンテキスト
+  const { getUser } = useUser();
+  const { userInfo } = useUserInfoContext();
 
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
@@ -32,24 +39,43 @@ export const useLoginWithGoogle = () => {
     });
     try {
       setError(false);
+      // ログイン
       const res = await signInWithPopup(fireauth, provider);
-      const isNewUser = getAdditionalUserInfo(res)?.isNewUser;
 
+      // --初めてのログインの場合・firebaseユーザ情報を利用してユーザDBを作成--
+      const isNewUser = getAdditionalUserInfo(res)?.isNewUser;
       if (isNewUser) {
         console.log("isNewUser");
-
         const userRegister: UserMinInfo = {
-          user_email: res.user.email ?? "",
-          user_name: res.user.displayName ?? "",
-          user_icon: res.user.photoURL ?? "",
-          user_id: res.user.uid ?? "",
+          user_email: res.user.email ?? undefined,
+          user_name: res.user.displayName ?? undefined,
+          user_icon: res.user.photoURL ?? undefined,
+          user_id: res.user.uid,
         };
         console.log(userRegister);
         await userCreateEdit("post", userRegister);
         navigate("/welcome");
       } else {
-        navigate("/");
+        // ユーザ情報をコンテキストにセット
+        getUser(res.user.uid);
+
+        // --初めてのログインではない場合・ユーザDBが存在すればトップページへ遷移する--
+        if (userInfo) {
+          navigate("/");
+        } else {
+          // --さらにユーザDBが存在しない場合・DB作成apiを叩いてwelcomeページへ遷移する--
+          const userRegister: UserMinInfo = {
+            user_email: res.user.email ?? undefined,
+            user_name: res.user.displayName ?? undefined,
+            user_icon: res.user.photoURL ?? undefined,
+            user_id: res.user.uid,
+          };
+          console.log(userRegister);
+          await userCreateEdit("post", userRegister);
+          navigate("/welcome");
+        }
       }
+
       setSuccess(true);
     } catch {
       console.log("ログインに失敗しました");
