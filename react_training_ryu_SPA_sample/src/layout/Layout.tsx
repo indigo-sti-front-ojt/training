b4030d8d99f08e5698538950a713f0e2576d1203
ReiRyuSTI <ry-tanaka@sios.com>
2022-09-02 14:30:50 +0900

    point data read move from layout to routerconfig

--- react_training_ryu_SPA_sample/src/layout/Layout.tsx
+++ react_training_ryu_SPA_sample/src/layout/Layout.tsx
@@ -1,57 +1,14 @@
-import React, { Suspense, useEffect } from "react";
+import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import { useAuthUser } from "../hocks/AuthUser";
-import { useImage } from "../hocks/Image";
-import { useShopDB } from "../hocks/ShopDB";
-import { useAreaTagDB } from "../hocks/AreaTagDB";
-import { useFreeTagDB } from "../hocks/FreeTagDB";
-import { useUserDB } from "../hocks/UserDB";
-import { AuthUserContainer } from "../provider/AuthUserProvider";
 
-import { UserDBContainer } from "../provider/UserDBProvider";
 import { FooterComponet } from "../designComponents/FooterComponent";
 import { HeaderComponent } from "../designComponents/HeaderComponent";
 import { LodingContainer } from "../provider/LoadingProvider";
-import { TagDBContainer } from "../provider/TagDBProvider";
-import { ImageContainer } from "../provider/ImageProvider";
 
 export const Layout = () => {
-  const { changeUserState } = useAuthUser();
-
-  const { FreeDataReads } = useFreeTagDB();
-  const { AreaDataReads } = useAreaTagDB();
-
-  const { UserDataRead } = useUserDB();
-  const { isLoggined, user } = AuthUserContainer.useContainer();
-  const { changeFlag } = UserDBContainer.useContainer();
-  const { TagChangeFlag } = TagDBContainer.useContainer();
-  const { ShopDataReads_ALL } = useShopDB();
-  const { imageEditFlag } = ImageContainer.useContainer();
-  const { imageDataReads } = useImage();
-
   const { loading } = LodingContainer.useContainer();
 
   const location = useLocation();
-
-  useEffect(() => {
-    changeUserState();
-    ShopDataReads_ALL();
-  }, []);
-
-  useEffect(() => {
-    FreeDataReads();
-    AreaDataReads();
-  }, [TagChangeFlag]);
-
-  useEffect(() => {
-    imageDataReads();
-  }, [imageEditFlag]);
-
-  useEffect(() => {
-    if (isLoggined) {
-      UserDataRead(user.uid);
-    }
-  }, [isLoggined, changeFlag]);
 
   return (
     <>
--- react_training_ryu_SPA_sample/src/router/RouterConfig.tsx
+++ react_training_ryu_SPA_sample/src/router/RouterConfig.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AboutPage } from "../views/AboutPage";
 import { HomePage } from "../views/HomePage";
@@ -24,7 +24,52 @@ import { RouteAuthGateReverse } from "./RouteAuthGateReverse";
 import { OwnerImageUp } from "../views/OwnerImageUp";
 import { ScrollTop } from "./ScrollTop";
 
+import { useAuthUser } from "../hocks/AuthUser";
+import { useImage } from "../hocks/Image";
+import { useShopDB } from "../hocks/ShopDB";
+import { useAreaTagDB } from "../hocks/AreaTagDB";
+import { useFreeTagDB } from "../hocks/FreeTagDB";
+import { useUserDB } from "../hocks/UserDB";
+import { AuthUserContainer } from "../provider/AuthUserProvider";
+
+import { UserDBContainer } from "../provider/UserDBProvider";
+import { TagDBContainer } from "../provider/TagDBProvider";
+import { ImageContainer } from "../provider/ImageProvider";
 export const RouterConfig = () => {
+  const { changeUserState } = useAuthUser();
+
+  const { FreeDataReads } = useFreeTagDB();
+  const { AreaDataReads } = useAreaTagDB();
+
+  const { UserDataRead } = useUserDB();
+  const { isLoggined, user } = AuthUserContainer.useContainer();
+  const { changeFlag } = UserDBContainer.useContainer();
+  const { TagChangeFlag } = TagDBContainer.useContainer();
+  const { ShopDataReads_ALL } = useShopDB();
+  const { imageEditFlag } = ImageContainer.useContainer();
+  const { imageDataReads } = useImage();
+  useEffect(() => {
+    changeUserState();
+    ShopDataReads_ALL();
+  }, []);
+
+  useEffect(() => {
+    FreeDataReads();
+    AreaDataReads();
+  }, [TagChangeFlag]);
+
+  useEffect(() => {
+    imageDataReads();
+  }, [imageEditFlag]);
+
+  useEffect(() => {
+    if (isLoggined) {
+      UserDataRead(user.uid);
+    }
+  }, [isLoggined, changeFlag]);
+
+  console.log("rend");
+
   return (
     <>
       <BrowserRouter>
